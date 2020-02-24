14a9d7a7b8ffeee58cc1c67d97dfe3a3566bf78a
Lara <gimena.befer@gmail.com>
2020-02-24 10:07:18 -0300

    upload projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarClientComponent } from './components/navbar-client/navbar-client.component';
+import { FormComponent } from './components/form/form.component';
+import { NavbarAdminComponent } from './components/navbar-admin/navbar-admin.component';
+import { SearchComponent } from './components/search/search.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { CompanyComponent } from './components/company/company.component';
+import { AbmGridComponent } from './components/abm-grid/abm-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarClientComponent,
+    FormComponent,
+    NavbarAdminComponent,
+    SearchComponent,
+    ProductsComponent,
+    NotFoundComponent,
+    CompanyComponent,
+    AbmGridComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/abm-grid/abm-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-abm-grid',
+  templateUrl: './abm-grid.component.html',
+  styleUrls: ['./abm-grid.component.css']
+})
+export class AbmGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
