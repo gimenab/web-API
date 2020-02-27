3a3bb2930362a3a3734286894504381b79bcf46b
Gaston Vottero <gastonvottero@hotmail.com>
2020-02-27 09:45:29 -0300

    components abm

---
+++ src/app/components/abmcategories/abmcategories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-abmcategories',
+  templateUrl: './abmcategories.component.html',
+  styleUrls: ['./abmcategories.component.css']
+})
+export class ABMCategoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/abmcurrency/abmcurrency.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-abmcurrency',
+  templateUrl: './abmcurrency.component.html',
+  styleUrls: ['./abmcurrency.component.css']
+})
+export class ABMCurrencyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
