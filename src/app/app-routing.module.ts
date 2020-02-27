ce45a46bc97b93d2669dc24f1eb04ba2b8b3f29f
Gaston Vottero <gastonvottero@hotmail.com>
2020-02-27 10:11:55 -0300

    navbar admin and abm of category, currency and company

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { ABMCurrencyComponent } from './components/abmcurrency/abmcurrency.component';
+import { ABMCategoriesComponent } from './components/abmcategories/abmcategories.component';
 import { HomeAdminComponent } from './components/home-admin/home-admin.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { CompanyComponent } from './components/company/company.component';
@@ -7,6 +9,7 @@ import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductComponent } from './components/product/product.component';
+import { ABMCompaniesComponent } from './components/abmcompanies/abmcompanies.component';
 
 
 const routes: Routes = [
@@ -16,6 +19,9 @@ const routes: Routes = [
   {path:'product/:id',component:ProductComponent},
   {path:'company/:id',component:CompanyComponent},
   {path:'admin',component:HomeAdminComponent},
+  {path:'abmCompanies',component:ABMCompaniesComponent},
+  {path:'abmCategories',component:ABMCategoriesComponent},
+  {path:'abmCurrencies',component:ABMCurrencyComponent},
   {path:'**',component:NotFoundComponent}
 ];
 
