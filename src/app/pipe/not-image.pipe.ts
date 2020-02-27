8a4bc7a5635768d78d335884cbef47bd0ea148ee
Gaston Vottero <gastonvottero@hotmail.com>
2020-02-27 09:20:50 -0300

    error Pipe

--- src/app/pipe/not-image.pipe.ts
+++ src/app/pipe/not-image.pipe.ts
@@ -6,11 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class NotImagePipe implements PipeTransform {
 
   transform(value: any): any {
-    if (value = null) {
+    if (!value) {
       return value= "https://image.shutterstock.com/image-vector/page-not-found-concept-404-260nw-764438536.jpg"
     }
     else{
-      return null;
+      return value;
     }
 
   }
