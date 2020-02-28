ec6f4017174b079c925e14c9b112900658201b37
Gaston Vottero <gastonvottero@hotmail.com>
2020-02-28 12:49:53 -0300

    abm Currency

--- src/app/components/abmcurrency/abmcurrency.component.ts
+++ src/app/components/abmcurrency/abmcurrency.component.ts
@@ -1,6 +1,8 @@
 import { CurrencyService } from './../../services/currency.service';
 import { Currency } from './../../models/Currency';
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2'
+import 'sweetalert2/src/sweetalert2.scss'
 
 @Component({
   selector: 'app-abmcurrency',
@@ -9,19 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ABMCurrencyComponent implements OnInit {
 
-  currency:Currency;
+  currency:Currency= new Currency();
   currencies:Currency[];
 
   constructor(private currencyService:CurrencyService) { }
 
   ngOnInit(): void {
-    this.currencyService.getAll().subscribe(response=>{
-      this.currencies=<Currency[]>response;
+    // this.currencyService.getAll().subscribe(response=>{
+    //   this.currencies=<Currency[]>response;
+    // })
+  }
+
+  delete(id:number){
+    Swal.fire({
+      title: '¿Esta seguro que desae eliminar?',text:'Concidere que al eliminar afectara los productos que aun lo posean',
+      showClass: {
+        popup: 'animated fadeInDown faster'
+      },
+      hideClass: {
+        popup: 'animated fadeOutUp faster'
+      },
+      icon: 'warning',
+      confirmButtonText: 'Si',
+      cancelButtonText: 'No',
+      showCancelButton: true,
+      showCloseButton: true
     })
   }
 
-
   submit(f){
-
+    Swal.fire({
+      icon: 'success',
+      title: 'Se realizo la carga',
+      showConfirmButton: false,
+      timer: 1500
+    })
   }
 }
