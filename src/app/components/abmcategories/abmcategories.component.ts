5b85df9e4cc7303b44abcb1c8df4dd90050b22ba
Gimena Fernandez <gimena.befer@gmail.com>
2020-02-28 13:12:33 -0300

    category ts is declared and html is commented

--- src/app/components/abmcategories/abmcategories.component.ts
+++ src/app/components/abmcategories/abmcategories.component.ts
@@ -1,3 +1,5 @@
+import { CategoryService } from './../../services/category.service';
+import { Categories } from './../../models/categories';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./abmcategories.component.css']
 })
 export class ABMCategoriesComponent implements OnInit {
+  category:Categories = new Categories();
+  categories:Categories[];
 
-  constructor() { }
+
+  constructor(private categoryService: CategoryService)
+     {
+
+   }
 
   ngOnInit(): void {
+    this.categoryService.getAll().subscribe( response =>{
+      this.categories=<Categories[]>response;
+    })
   }
+  submit(f){
 
+  }
 }
