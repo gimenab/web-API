1f992f20d6e501f91d6ffebe2a75a043fa0ef56f
Gimena Fernandez <gimena.befer@gmail.com>
2020-03-02 11:30:15 -0300

    are defined add required to category name and add list with directives

--- src/app/components/abmcategories/abmcategories.component.ts
+++ src/app/components/abmcategories/abmcategories.component.ts
@@ -2,6 +2,7 @@ import { CategoryService } from './../../services/category.service';
 import { Categories } from './../../models/categories';
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-abmcategories',
   templateUrl: './abmcategories.component.html',
@@ -10,6 +11,9 @@ import { Component, OnInit } from '@angular/core';
 export class ABMCategoriesComponent implements OnInit {
   category:Categories = new Categories();
   categories:Categories[];
+  categoryFather:Categories[];
+  selectCategory = [{string : 'categoryName'}];
+
 
 
   constructor(private categoryService: CategoryService)
@@ -21,7 +25,21 @@ export class ABMCategoriesComponent implements OnInit {
     this.categoryService.getAll().subscribe( response =>{
       this.categories=<Categories[]>response;
     })
+    this.categoryService.get('/api/Categories/father').subscribe( response =>{
+      this.categoryFather=<Categories[]>response;
+    })
   }
+  create(){
+    this.category=new Categories();
+  }
+
+  update(){
+
+  }
+  delete(){
+
+  }
+
   submit(f){
 
   }
