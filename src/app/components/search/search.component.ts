e7ec3ac3adb98ea3ee36976b543c39c5021f7693
Gaston Vottero <gastonvottero@hotmail.com>
2020-03-03 17:14:06 -0300

    search and error correction carousel

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,7 @@
+import { Products } from 'src/app/models/products';
+import { ProductsService } from './../../services/products.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:ActivatedRoute, private productsService:ProductsService) { }
+
+  productSearch:string;
+  products:Products[]=[];
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(response=>{
+      this.productSearch=response["params"]["search"];
+      this.search();
+    })
   }
 
+  search(){
+    this.productsService.get('api/Products/search?orderBy=ProductName&value='+ this.productSearch)
+        .subscribe(response=>{
+          this.products=<Products[]>response;
+        })
+  }
 }
